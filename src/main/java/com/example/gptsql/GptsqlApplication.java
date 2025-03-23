37eb2e1cbda7884f1029a0e05835240821f7518f
hwoo <tone8943@gmail.com>
2025-03-23 15:22:13 +0900

    Initial commit

---
+++ src/main/java/com/example/gptsql/GptsqlApplication.java
@@ -0,0 +1,13 @@
+package com.example.gptsql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GptsqlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GptsqlApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/gptsql/GptsqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gptsql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GptsqlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
